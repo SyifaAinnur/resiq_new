687f36ba60a7f0b8da980fac69a6929a481547eb
ekkitaufikm <79741859+ekkitaufikm@users.noreply.github.com>
2021-09-08 16:55:47 +0700

    update login register

--- app/Views/admin/use/login.php
+++ app/Views/admin/use/login.php
@@ -7,9 +7,8 @@
     <meta name="description" content="Cuba admin is super flexible, powerful, clean &amp; modern responsive bootstrap 5 admin template with unlimited possibilities.">
     <meta name="keywords" content="admin template, Cuba admin template, dashboard template, flat admin template, responsive admin template, web app">
     <meta name="author" content="pixelstrap">
-    <link rel="icon" href="<?= base_url(); ?>/assets/admin/images/favicon.png" type="image/x-icon">
-    <link rel="shortcut icon" href="<?= base_url(); ?>/assets/admin/images/favicon.png" type="image/x-icon">
     <title>Admin Resiq - Login</title>
+    <link rel="icon" href="<?= base_url(); ?>/assets/assets-landing/images/favicon.png">
     <!-- Google font-->
     <link href="https://fonts.googleapis.com/css?family=Rubik:400,400i,500,500i,700,700i&amp;display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900&amp;display=swap" rel="stylesheet">
