562c44add72ee2343254d628dd3cc1f3d6c1d1fd
Syifa Ainnur <arwahitem01@gmail.com>
2021-09-08 14:39:37 +0700

    Update MainLayout.php

--- app/Views/landing_page/layout/MainLayout.php
+++ app/Views/landing_page/layout/MainLayout.php
@@ -33,13 +33,13 @@
 
 <body>
     <!-- start cssload-loader -->
-    <!-- <div class="preloader" id="preloader">
+    <div class="preloader" id="preloader">
         <div class="loader">
             <svg class="spinner" viewBox="0 0 50 50">
                 <circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>
             </svg>
         </div>
-    </div> -->
+    </div>
 
     <?= $this->include('landing_page/layout/navbar') ?>
 
